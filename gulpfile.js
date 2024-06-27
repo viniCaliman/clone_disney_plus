0016e5236f329a56af8fedd0f1efdc415afcd06f
Vinicius Paiva Caliman <vinicaliman@gmail.com>
2024-06-27 12:23:10 -0300

    feat: criação do hero, inclusão das imagens e fonts do projeto. setup: Ajuste no package.json para segregar os scripts de dev e build e ajuste na versão do gulp para evitar corrompimento das imagens.

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,26 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
+const sourceMaps = require('gulp-sourcemaps');
 
-function styles (){
+function styles() {
     return gulp.src('./src/styles/*.scss')
-        .pipe(sass({ outputStyle: 'compressed'}))
-        .pipe(gulp.dest('./dist/css'));
+    .pipe(sourceMaps.init())
+    .pipe(sass({
+        outputStyle: 'compressed'
+    }))
+    .pipe(sourceMaps.write('./maps'))
+    .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = styles;
+function images() {
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
+}
+
+exports.default = gulp.parallel(styles, images);
+
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
